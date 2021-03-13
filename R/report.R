b00d6269890bbbbb062694d79c6cd0d6d154a500
Marko Smiljanic <marko.smiljanic313@gmail.com>
2021-03-13 18:24:53 +0100

    Multiple cleaning scripts

--- R/report.R
+++ R/report.R
@@ -16,10 +16,12 @@
 #'
 #' @export
 
-report <- function(timeindex, cleaning_script, plot_name, filename = NULL, scales = 'fixed') {
+report <- function(timeindex, cleaning_script, plot_name, filename = NULL, ...) {
   out <- timeindex
-  source(cleaning_script, local = TRUE)
-  out %>% report_plot(facets=T, scales) %>% ggplot2::ggsave(plot_name, plot=., width=20, height=12, scale = 0.5)
+  for(i in 1:length(cleaning_script)) {
+    source(cleaning_script[[i]], local = TRUE)
+  }
+  out %>% report_plot(facets=T, ...) %>% ggplot2::ggsave(plot_name, plot=., width=20, height=12, scale = 0.5)
   if(!is.null(filename)) {
     readr::write_csv(out, filename)
   }
