8bfa4929947c35595f9823001d4a4ce116936f7b
Marko Smiljanic <marko.smiljanic313@gmail.com>
2025-02-12 11:42:16 +0100

    edge case if min_res = 0. Possible duplicates in the original data

--- R/jump_correction.R
+++ R/jump_correction.R
@@ -33,16 +33,17 @@ jump_correction <- function(data, pre, post, jump, tz='UTC', offset = 0, name = 
   }
   min_res <- min_resolution(data)
 
-  # print(min_res)
+  # print(min_res) should be a warning of duplicates if min_res = 0
+  if(min_res > 0) {
+    pre[[1]] <- lubridate::as_datetime(as.numeric(pre[[1]]) %/% min_res * min_res, tz=tz)
+    pre[[2]] <-  lubridate::as_datetime(as.numeric(pre[[2]] + min_res) %/% min_res * min_res, tz=tz)
 
-  pre[[1]] <- lubridate::as_datetime(as.numeric(pre[[1]]) %/% min_res * min_res, tz=tz)
-  pre[[2]] <-  lubridate::as_datetime(as.numeric(pre[[2]] + min_res) %/% min_res * min_res, tz=tz)
+    post[[1]] <- lubridate::as_datetime(as.numeric(post[[1]]) %/% min_res * min_res, tz=tz)
+    post[[2]] <-  lubridate::as_datetime(as.numeric(post[[2]] + min_res) %/% min_res * min_res, tz=tz)
 
-  post[[1]] <- lubridate::as_datetime(as.numeric(post[[1]]) %/% min_res * min_res, tz=tz)
-  post[[2]] <-  lubridate::as_datetime(as.numeric(post[[2]] + min_res) %/% min_res * min_res, tz=tz)
-
-  jump[[1]] <- lubridate::as_datetime(as.numeric(jump[[1]]) %/% min_res * min_res, tz=tz)
-  jump[[2]] <-  lubridate::as_datetime(as.numeric(jump[[2]] + min_res) %/% min_res * min_res, tz=tz)
+    jump[[1]] <- lubridate::as_datetime(as.numeric(jump[[1]]) %/% min_res * min_res, tz=tz)
+    jump[[2]] <-  lubridate::as_datetime(as.numeric(jump[[2]] + min_res) %/% min_res * min_res, tz=tz)
+  }
 
   # print(jump)
   data[data$TIMESTAMP %>=<% jump, name] <- NA
