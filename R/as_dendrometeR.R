ad5423777fb23b8614fb36ee46b8fec4dd33f07a
Marko Smiljanic <marko.smiljanic313@gmail.com>
2025-02-12 11:40:52 +0100

    as_dendrometeR edit

--- R/as_dendrometeR.R
+++ R/as_dendrometeR.R
@@ -13,15 +13,25 @@
 #'
 #' @export
 as_dendrometeR <- function(data, timestamp_column = 1) {
+  data <- data %>% arrange(pick((timestamp_column)))
   out <- as.data.frame(data, stringsAsFactors = FALSE)
-  timestamps <- data[timestamp_column]
-  timestmaps_diffs <- unique(diff(timestamps))
+  timestamps <- data[[timestamp_column]] %>% as.vector()
+  unq_timestamps <- timestamps %>% diff() %>% unique()
 
-  #if() {
-
-#  }
-  rownames(out) <- as.characater(data[timestamp_column])
-  out <- out[-timestamp_column]
+  if(length(unq_timestamps) != 1) {
+    min_timestamp = min(timestamps, na.rm =T)
+    max_timestamp = max(timestamps, na.rm =T)
+    timestamps = seq(min_timestamp, max_timestamp, min(unq_timestamps))
+  } else {
+    timestamps = data[[timestamp_column]] %>% as.vector()
+  }
+  timestamp_name = colnames(data)[[timestamp_column]]
+  # print(timestamp_name)
+  ts_df <- data.frame(as.POSIXct(timestamps))
+  colnames(ts_df) <- timestamp_name
+  # print(ts_df %>% head())
+  out <- ts_df %>% left_join(out) %>% tibble::column_to_rownames(var = timestamp_name)
+  # print(out %>% head())
   out
 }
 
