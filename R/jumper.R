36488b9c40620f28720af115a1530e97cd30de68
Marko Smiljanic <marko.smiljanic313@gmail.com>
2025-06-03 08:35:39 +0200

    recursive jump correction

---
+++ R/jumper.R
@@ -0,0 +1,37 @@
+#' Recursive jump correction
+#'
+#' @description auto correct jump and outlier corrections
+#'
+#' @usage jumper(data, timestamp_column = 1, name = 'Sensor', ...)
+#'
+#' @return A \code{data.frame}
+#'
+#' @author Marko Smiljanic
+#'
+#' @examples
+#'
+#' @importFrom tidyr `%>%`
+#' @importFrom RCop `%><%`
+#' @importFrom RCop `%+=%`
+#'
+#' @export
+jumper <- function (data, timestamp_column = 1, name = 'Sensor', ...) {
+  if(is.numeric(name)) {
+    name <- colnames(data)[name]
+  }
+
+  if (!inherits(data[[timestamp_column]], "Date") && !inherits(data[[timestamp_column]], "POSIXct")) {
+    data[[timestamp_column]] <- ymd_hms(data[[timestamp_column]])
+  }
+
+  df_out <- auto_jump(data, timestamp_column = timestamp_column, name = name, ...)
+  print(df_out)
+  correction <- df_out$TIMESTAMP[1]
+  data <- data %>% jump_correction(jump = correction)
+  df_out <- auto_jump(data, timestamp_column = timestamp_column, name = name, ...)
+  if(nrow(df_out) == 0) {
+    return(data)
+  } else {
+    jumper(data, timestamp_column, name, ...)
+  }
+}
